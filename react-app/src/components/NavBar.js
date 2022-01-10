7ed1559733bc3402bf34061b5cf4a1cf17f8c2a8
kstonekuan <kingstonkuan@u.nus.edu>
2022-01-10 22:01:38 +0800

    get userinfo from props

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,27 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = (props) => {
-  const [userInfo, setUserInfo] = useState();
+  // const [userInfo, setUserInfo] = useState();
   const providers = ['twitter', 'github', 'aad'];
   const redirect = window.location.pathname;
 
-  useEffect(() => {
-    (async () => {
-      setUserInfo(await getUserInfo());
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     setUserInfo(await getUserInfo());
+  //   })();
+  // }, []);
 
-  async function getUserInfo() {
-    try {
-      const response = await fetch('/.auth/me');
-      const payload = await response.json();
-      const { clientPrincipal } = payload;
-      return clientPrincipal;
-    } catch (error) {
-      console.error('No profile could be found');
-      return undefined;
-    }
-  }
+  // async function getUserInfo() {
+  //   try {
+  //     const response = await fetch('/.auth/me');
+  //     const payload = await response.json();
+  //     const { clientPrincipal } = payload;
+  //     return clientPrincipal;
+  //   } catch (error) {
+  //     console.error('No profile could be found');
+  //     return undefined;
+  //   }
+  // }
 
   return (
     <div className="column is-2">
@@ -59,22 +59,22 @@ const NavBar = (props) => {
       <nav className="menu auth">
         <p className="menu-label">Auth</p>
         <div className="menu-list auth">
-          {!userInfo &&
+          {!props.userInfo &&
             providers.map((provider) => (
               <a key={provider} href={`/.auth/login/${provider}?post_login_redirect_uri=${redirect}`}>
                 {provider}
               </a>
             ))}
-          {userInfo && <a href={`/.auth/logout?post_logout_redirect_uri=${redirect}`}>Logout</a>}
+          {props.userInfo && <a href={`/.auth/logout?post_logout_redirect_uri=${redirect}`}>Logout</a>}
         </div>
       </nav>
       {
-        userInfo && (
+        props.userInfo && (
           <div>
             <div className="user">
               <p>Welcome</p>
-              <p>{userInfo && userInfo.userDetails}</p>
-              <p>{userInfo && userInfo.identityProvider}</p>
+              <p>{props.userInfo && props.userInfo.userDetails}</p>
+              <p>{props.userInfo && props.userInfo.identityProvider}</p>
             </div>
           </div>
         )
