eacb3cf459bdb388d05177a3815072709c17ec8a
kstonekuan <kingstonkuan@u.nus.edu>
2022-01-26 19:11:00 +0800

    add delete and update sessions apis

--- api/sessions-delete/index.js
+++ api/sessions-delete/index.js
@@ -7,10 +7,8 @@ module.exports = async function (context, req) {
         const pool = await sql.connect(AZURE_CONN_STRING);
 
         const res = await pool.query(`
-            update Profiles
-            set Name = '${req.body.name}', Email = '${req.body.email}', 
-                Mobile = '${req.body.mobile}', Social = '${req.body.social}', Summary = '${req.body.summary}'
-            where ProfileId = ${parseInt(req.params.id, 10)};
+            delet from Sessions
+            where SessionId = ${parseInt(req.params.id, 10)};
         `);
 
         context.log(res);
---
+++ api/sessions-put/index.js
@@ -0,0 +1,24 @@
+const sql = require('mssql')
+
+const AZURE_CONN_STRING = process.env["AzureSQLConnectionString"];
+
+module.exports = async function (context, req) {    
+    try {
+        const pool = await sql.connect(AZURE_CONN_STRING);
+
+        const res = await pool.query(`
+            update Sessions
+            set Link = '${req.body.link}', TimeSlot = '${req.body.timeslot}', 
+                LenghtMinutes = '${req.body.lengthMinutes}', CompletionStatus = '${req.body.status}',
+                MentorAgenda = '${req.body.mentor.agenda}', MentorFeedback = '${req.body.mentor.feedback}',
+                MenteeAgenda = '${req.body.mentee.agenda}', MenteeFeedback = '${req.body.mentee.feedback}'
+            where SessionId = ${parseInt(req.params.id, 10)};
+        `);
+
+        context.log(res);
+
+        context.res.status(200);
+    }  catch (error) {
+        context.res.status(500).send(error);
+    }
+}
